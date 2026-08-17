30dc1bd12d339eb7b70382d7a2d41d1b992b38ea
Shivam Giri <shivamgiri2015@gmail.com>
2026-08-17 13:14:42 +0200

    [BA] Optimize taskflow repository.

--- server/src/main/java/com/example/taskflow/Controller/TaskflowController.java
+++ server/src/main/java/com/example/taskflow/Controller/TaskflowController.java
@@ -1,4 +1,4 @@
-package com.example.taskflow;
+package com.example.taskflow.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
